1e852a04854c5db7234044dee7c680a9e333bcba
octavioromagnoli <octaromag@gmail.com>
2024-07-25 19:58:16 -0300

    Add operate function

--- scripts.js
+++ scripts.js
@@ -9,3 +9,28 @@ function substract(a,b){
 function multiply(a,b){
   return a * b;
 }
+
+function divide(a,b){
+  return a / b;
+}
+
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+function operate(num1, num2, operator){
+  switch(operator){
+    case "+":
+      sum(num1, num2);
+      break;
+    case "-":
+      substract(num1, num2);
+      break;
+    case "*":
+      multiply(num1, num2);
+      break;
+    case "/":
+      divide(num1, num2);
+      break;
+  }
+}
